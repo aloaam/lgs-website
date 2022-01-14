dfa0a56e7139740f0b71c98ea93265a0d2e2b5c4
aloariza <alariza@deloitte-audit-analytics.com>
2022-01-14 23:00:01 +0100

    landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import LandingPageImage from "../assets/images/casa_de_lado.jpeg";
+import ProductImage from "../assets/images/pvc_landing_page.png";
 
 export default function Home() {
   return (
     <div className="home">
-      <img
-        className="home_image"
-        style={{ backgroundImage: `url(${LandingPageImage})` }}
-      />
-
-      <div className="text-container">
-        <h1> Leslie Glass Suppliers</h1>
-        <p> We are currently under construction, but please check us out!</p>
-        <Link to="products">
-          <button> Products </button>
-        </Link>
+      <div className="text-and-product">
+        <div className="text-container">
+          <h1> Leslie Glass Suppliers</h1>
+          <p> We are currently under construction, but please check us out!</p>
+          <Link to="products">
+            <button> Products </button>
+          </Link>
+        </div>
+        <div
+          className="product-image"
+          style={{ backgroundImage: `url(${ProductImage})` }}
+        ></div>
       </div>
     </div>
   );
