8ef74ba279b39b6d766ad3f9fe2e38069818beef
aloariza <alariza@deloitte-audit-analytics.com>
2022-01-16 17:47:54 +0100

    minor fixes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,7 +9,9 @@ export default function Home() {
       <div className="text-and-product">
         <div className="text-container">
           <h1> Leslie Glass Suppliers</h1>
-          <p> We are currently under construction, but please check us out!</p>
+          <p>
+            Our site is currently under construction, but please check us out!
+          </p>
           <Link to="products">
             <button> Products </button>
           </Link>
