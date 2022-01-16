12b3fc902890730b003a17d024bf42f279564665
aloariza <alariza@deloitte-audit-analytics.com>
2022-01-16 19:28:15 +0100

    add first version of carousel to products

---
+++ src/components/CarouselItem.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CarouselItem() {
+  return <div className="products__carousel-item"></div>;
+}
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,15 +1,16 @@
 import React from "react";
+import Carousel from "react-elastic-carousel";
 import { productList } from "../helpers/ProductList";
 import Product from "../components/Product";
 
 export default function Products() {
   return (
     <div className="products">
-      <div className="menuList">
+      <Carousel className="company_carousel-products">
         {productList.map((pizza, key) => {
           return <Product key={key} {...pizza} />;
         })}
-      </div>
+      </Carousel>
     </div>
   );
 }
