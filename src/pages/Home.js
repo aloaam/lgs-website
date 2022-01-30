308a7a63dd2062321ab01dc4bb062a9b8ba6ec17
aloariza <alariza@deloitte-audit-analytics.com>
2022-01-30 20:22:18 +0100

    mobile version for Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,10 +8,14 @@ export default function Home() {
     <div className="home">
       <div className="text-and-product">
         <div className="text-container">
-          <h1> Leslie Glass Suppliers</h1>
-          <p>
-            Our site is currently under construction, but please contact us!
-          </p>
+          <div className="text-container__title">
+            <h1>Leslie Glass Suppliers</h1>
+          </div>
+          <div className="text-container__text">
+            <p>
+              Our site is currently under construction, but please contact us!
+            </p>
+          </div>
           <div className="text-container__button">
             <Link to="get-a-quote">
               <button> Contact </button>
